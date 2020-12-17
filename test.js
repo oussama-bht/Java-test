4d2541c4118499963e9d23e74ae2ed0613c785f1
oussama-bht <boumhaout8@gmail.com>
2020-12-17 19:32:59 +0100

    helo

---
+++ test.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.prod1').show();
+    $(".prod2").hide();
+    $('#prod1').addClass('active');
+    $('.tabs button').on('click',function(){
+        $(".tabs button").removeClass("active");
+        if ( !$(this).hasClass("active") ){
+            $(this).addClass("active");
+        } else {
+            $(this).removeClass("active");
+        }
+        $(".prod").hide();
+        $("."+this.id).show();
+    });
+});
