40fd8e4fef207eba88c8fec585c3e588489f9c5f
oussama-bht <boumhaout8@gmail.com>
2020-12-18 21:54:10 +0100

    helo

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
         if ( !$(this).hasClass("active") ){
             $(this).addClass("active");
         } else {
+            console.log();
             $(this).removeClass("active");
         }
         $(".prod").hide();
